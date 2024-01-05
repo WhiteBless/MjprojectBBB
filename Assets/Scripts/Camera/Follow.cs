8e7684a678cd99bc042f1cd57b8e8e8913f072f4
Ji_Won <121164034+NickJeongWib@users.noreply.github.com>
2024-01-05 21:45:42 +0900

    2024_01_05
    1. 3번 찍기 이펙트 수정 및 코드 수정
    2. 폴더 정리

--- Assets/Scripts/Camera/Follow.cs
+++ Assets/Scripts/Camera/Follow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CaneraController : MonoBehaviour
 {
@@ -26,20 +27,26 @@ public class CaneraController : MonoBehaviour
     // 매 프레임마다 갱신 
     void LateUpdate()
     {
-        if (_mode == Define.CameraMode.QarterView && _player != null)
+        if (!(SceneManager.GetActiveScene().name == "Main"))
         {
-            RaycastHit hit;
-            if (Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))
-            {
-                float dist = (hit.point - _player.transform.position).magnitude * wallDistance;
-                transform.position = _player.transform.position + _delta.normalized * dist;
-            }
-            else
-            {
-                transform.position = _player.transform.position + _delta;
-                transform.LookAt(_player.transform);
-            }
+            transform.position = _player.transform.position + _delta;
+            transform.LookAt(_player.transform);
         }
+
+        //if (_mode == Define.CameraMode.QarterView && _player != null)
+        //{
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))
+        //    {
+        //        float dist = (hit.point - _player.transform.position).magnitude * wallDistance;
+        //        transform.position = _player.transform.position + _delta.normalized * dist;
+        //    }
+        //    else
+        //    {
+        //        transform.position = _player.transform.position + _delta;
+        //        transform.LookAt(_player.transform);
+        //    }
+        //}
     }
     public void SetQuaterView(Vector3 delta)
     {
