6dd06940e4e03edd2cd18182402edd8c4975056f
ManJin12 <127113139+ManJin12@users.noreply.github.com>
2024-02-02 23:36:22 +0900

    Revert "0202"
    This reverts commit 2a4df7e3ee24115b872b34af1cf7e883a1a5ec6d.

--- Assets/Scripts/KeySetting/KeyManager.cs
+++ Assets/Scripts/KeySetting/KeyManager.cs
@@ -4,49 +4,30 @@ using UnityEngine;
 
 public enum KeyAction { Skill1, Skill2, Skill3, Skill4, Dodge, KeyCount }
 
-public static class KeySetting
-{
-    public static Dictionary<KeyAction, KeyCode> Keys = new Dictionary<KeyAction, KeyCode>();
-}
-
+public static class KeySetting { public static Dictionary<KeyAction, KeyCode> Keys = new Dictionary<KeyAction, KeyCode>(); }
 public class KeyManager : MonoBehaviour
 {
-    KeyCode[] defaultKeys = new KeyCode[] { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.Space };
+    int key = -1;
 
+    KeyCode[] defaultKeys = new KeyCode[] { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.Space };
     private void Awake()
     {
-        for (int i = 0; i < (int)KeyAction.KeyCount; i++)
+        for(int i= 0; i<(int)KeyAction.KeyCount; i++)
         {
-            if (!KeySetting.Keys.ContainsKey((KeyAction)i)) // 중복된 키가 아니라면 추가
-            {
-                KeySetting.Keys.Add((KeyAction)i, defaultKeys[i]);
-            }
-        }
+            KeySetting.Keys.Add((KeyAction)i, defaultKeys[i]);
+        } 
     }
 
     private void OnGUI()
     {
         Event keyEvent = Event.current;
 
-        if (keyEvent.isKey && key != -1)
+        if(keyEvent.isKey)
         {
-            KeyAction keyAction = (KeyAction)key;
-
-            // 중복된 키가 없으면 추가
-            if (!KeySetting.Keys.ContainsValue(keyEvent.keyCode))
-            {
-                KeySetting.Keys[keyAction] = keyEvent.keyCode;
-            }
-            else
-            {
-                Debug.LogWarning("이미 사용 중인 키입니다.");
-            }
-
+            KeySetting.Keys[(KeyAction) key] = keyEvent.keyCode;
             key = -1;
         }
     }
-
-    int key = -1;
 
     public void ChangeKey(int num)
     {
