ae59985fd4df69c405ead3f82b1788785d007600
ManJin12 <127113139+ManJin12@users.noreply.github.com>
2024-03-12 22:36:06 +0900

    3/12

--- Assets/Scripts/CharacterCtrl/Assassin/ShurikenController.cs
+++ Assets/Scripts/CharacterCtrl/Assassin/ShurikenController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ShurikenController : MonoBehaviour
 {
-    public float moveSpeed = 10f; // 이동 속도
-    public float maxDistance = 30f; // 최대 이동 거리
+    public float moveSpeed; // 이동 속도
+    public float maxDistance; // 최대 이동 거리
 
     void Start()
     {
@@ -23,7 +23,7 @@ public class ShurikenController : MonoBehaviour
         transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
 
         // 발사체 이동 거리 체크
-        if (Vector3.Distance(transform.position, transform.parent.parent.parent.GetChild(0).position) > maxDistance)
+        if (Vector3.Distance(transform.position, transform.parent.parent.parent.GetChild(1).position) > maxDistance)
         {
             gameObject.SetActive(false); // 일정 거리 이동 후 비활성화
         }
