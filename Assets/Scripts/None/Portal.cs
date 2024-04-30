4872554da21b282fc17c5df7f6426de1365b0f06
WhiteBless <100004187+WhiteBless@users.noreply.github.com>
2024-04-30 15:50:13 +0900

    24/04/30 KRH

--- Assets/Scripts/None/Portal.cs
+++ Assets/Scripts/None/Portal.cs
@@ -4,31 +4,17 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    public GameObject targetObj;
-    public GameObject toObj;
+    public GameObject toObj; // 워프 포인트 오브젝트
 
-    public AudioSource audioSource; // 효과음을 재생할 AudioSource
-    public AudioClip portalSound; // 포탈 효과음
-
-    public void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("P"))
+        if (other.CompareTag("WarpPlayer"))
         {
-            // 플레이어 이동
-            targetObj.transform.position = toObj.transform.position;
+            // 플레이어를 워프 포인트 위치로 이동
+            other.transform.position = toObj.transform.position;
 
-            // 자식 오브젝트들도 같은 위치로 이동
-            foreach (Transform childTransform in targetObj.transform)
-            {
-                childTransform.position = toObj.transform.position;
-            }
-        }
-        if (other.CompareTag("Player"))
-        {
-            targetObj = other.gameObject;
-
-            // 포탈 효과음 재생
-            audioSource.PlayOneShot(portalSound, 1.0f); // 포탈 효과음 재생 (볼륨은 1.0)
+            // 포탈 효과음 재생 로직은 유지
+            //audioSource.PlayOneShot(portalSound, 1.0f); // 포탈 효과음 재생 코드
         }
     }
 }
