680e37e18f241aef1dd6ea29dd4269a23f7f5885
WhiteBless <100004187+WhiteBless@users.noreply.github.com>
2025-01-07 19:30:25 +0900

    2025-01-07 KRH
    맵작업 진행중. 포탈 연결 (컷씬X)

--- Assets/Scripts/Manager/PortManamger.cs
+++ Assets/Scripts/Manager/PortManamger.cs
@@ -2,40 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PortManager : MonoBehaviour
 {
-    void Start()
-    {
-        // 게임 시작 시 모든 포탈 비활성화
-        //foreach (var pair in bossPortalPairs)
-        //{
-        //    pair.portal.SetActive(false);
-        //}
-    }
 
-    void Update()
-    {
-        //foreach (var pair in bossPortalPairs)
-        //{
-        //    if (pair.boss == null || pair.portal == null)
-        //    {
-        //        Debug.LogError("PortManager: BossPortalPair에 null 객체가 있습니다.");
-        //        continue;
-        //    }
+    //void Start()
+    //{
+        //게임 시작 시 모든 포탈 비활성화
+    //    foreach (var pair in bossPortalPairs)
+    //    {
+    //        pair.portal.SetActive(false);
+    //    }
+    //}
 
-        //    Boss_HP_Controller bossHpController = pair.boss.GetComponent<Boss_HP_Controller>();
-        //    if (bossHpController == null)
-        //    {
-        //        Debug.LogError("PortManager: Boss_HP_Controller 컴포넌트를 찾을 수 없습니다.");
-        //        continue;
-        //    }
+    //void Update()
+    //{
+    //    foreach (var pair in bossPortalPairs)
+     //   {
+     //       if (pair.boss == null || pair.portal == null)
+     //       {
+    //            Debug.LogError("PortManager: BossPortalPair에 null 객체가 있습니다.");
+    //            continue;
+    //        }
 
-        //    if (bossHpController.isDead && !pair.portal.activeInHierarchy)
-        //    {
-        //        // 해당 보스가 죽었고, 포탈이 아직 활성화되지 않았다면 포탈 활성화
-        //        pair.portal.SetActive(true);
-        //    }
-        //}
-    }
+    //        Boss_HP_Controller bossHpController = pair.boss.GetComponent<Boss_HP_Controller>();
+    //        if (bossHpController == null)
+    //        {
+    //            Debug.LogError("PortManager: Boss_HP_Controller 컴포넌트를 찾을 수 없습니다.");
+    //            continue;
+    //        }
+
+    //        if (bossHpController.isDead && !pair.portal.activeInHierarchy)
+    //        {
+    //            // 해당 보스가 죽었고, 포탈이 아직 활성화되지 않았다면 포탈 활성화
+    //            pair.portal.SetActive(true);
+    //        }
+    //    }
+   // }
 }
 
